43087dfbb879253d77fc0f524cec6645f8fa4990
santiago Yepes T <zetogk@gmail.com>
2018-12-01 14:29:24 -0500

    feat: the method listMethods was added

--- index.js
+++ index.js
@@ -13,15 +13,32 @@ class GRPCClient {
             oneofs: true
         });
 
-        this.proto = grpc.loadPackageDefinition(this.packageDefinition)[packageName];
+        const proto = grpc.loadPackageDefinition(this.packageDefinition)[packageName];
 
-        this.client = new this.proto[service](host, grpc.credentials.createInsecure());
+        const listMethods = this.packageDefinition[`${packageName}.${service}`];
+
+        this.client = new proto[service](host, grpc.credentials.createInsecure());
+
+        this.listNameMethods = [];
+
+        for (const key in listMethods) {
+
+            const methodName = listMethods[key].originalName;
+            this.listNameMethods.push(methodName);
+
+        }
 
     }
 
     runService(fnName, data, fnAnswer) {
 
         this.client[fnName](data, fnAnswer);
+
+    }
+
+    listMethods() {
+
+        return this.listNameMethods;
 
     }
 
